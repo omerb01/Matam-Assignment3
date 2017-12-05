5d9df55396dfc107fb7d3ea5e05249f1de48e3df
Omer <omer.be@campus.technion.ac.il>
2017-12-05 22:36:48 +0200

    updated node's header

--- node_mtm.h
+++ node_mtm.h
@@ -19,77 +19,65 @@ typedef struct Node_t *Node;
 
 
 typedef enum {
-    NODE_OK, NODE_MEMORY_ERROR, NODE_ILLEGAL_INDEX
+    NODE_OK, NODE_ELEMENT_OVERIDE
 } NodeResult;
 
 //------------------------------------------------------------------------------------------
 // Create a new node and connects a given element
-// uses duplication for the given element
+// checks by assert element != NULL
+// makes duplication for the given element
 // returns the new node or NULL if can not create one
 Node nodeCreate(NodeElement element, compareNodeElement, displayNodeElement, copyNodeElement);
 
 //------------------------------------------------------------------------------------------
-// add a given node in a position that appears
-// just before a given existing index. First index is 0.
-// the added element is inserted by a simple assignment (without duplication).
-// return values : NODE_OK, NODE_MEMORY_ERROR, NODE_ILLEGAL_INDEX
-NodeResult nodeAddNodeElementBefore(Node, NodeElement, int);
+// returns the element that stored by a given node
+// element could not be NULL
+NodeElement nodeGetElement(Node node);
 
 //------------------------------------------------------------------------------------------
-// add a given element in a position at the start
-// the added element is inserted by a simple assignment (without duplication).
-// return values : NODE_OK, NODE_MEMORY_ERROR.
-NodeResult nodeAddNodeElementStart(Node, NodeElement);
+// connects a given "next node" to the next node
+// if node's next != NULL returns NODE_ELEMENT_OVERIDE error
+// checks by assert node != NULL && next != NULL
+// no need for duplication or destroy
+// returns NODE_OK, NODE_ELEMENT_OVERIDE
+NodeResult nodeAddNext(Node node, Node next);
 
 //------------------------------------------------------------------------------------------
-// add a given element in a position at the end
-// the added element is inserted by a simple assignment (without duplication).
-// return values : NODE_OK, NODE_MEMORY_ERROR.
-NodeResult nodeAddNodeElementEnd(Node, NodeElement);
+// connects a given "previous node" to the previous node
+// if node's next != NULL returns NODE_ELEMENT_OVERIDE error
+// checks by assert node != NULL && next != NULL
+// no need for duplication or destroy
+// returns NODE_OK, NODE_ELEMENT_OVERIDE
+NodeResult nodeAddPrevious(Node node, Node previous);
 
 //------------------------------------------------------------------------------------------
-// result_index gets an index at which there exists an element that is equal 
-// to a given element by compare function
-// the result index is the first (lowest) index that is equal or greater to base_index.
-// if No element is found, then result_index gets -1.
-// node ,element and result_index must not be NULL. (checked by assert).
-// return values : NODE_OK, NODE_ILLEGAL_INDEX.
-NodeResult
-nodeIndexOfNodeElement(Node node, NodeElement element, int base_index,
-                       compareNodeElement,
-                       int *result_index);
+// initilize the next node by NULL
+// no need for destroy
+// returns NODE_OK
+NodeResult nodeRemoveNext(Node node);
 
 //------------------------------------------------------------------------------------------
-// Delete an element at a given position (index)
-// return values : NODE_OK, NODE_MEMORY_ERROR , NODE_ILLEGAL_INDEX
-NodeResult nodeRemoveNodeElement(Node, int);
+// initilize the previous node by NULL
+// no need for destroy
+// returns NODE_OK
+NodeResult nodeRemovePrevious(Node node);
 
 //------------------------------------------------------------------------------------------
 // update an existing element in a given position (index) to be identical
 // to a given element. the given element must not be changed.
 // no need to duplicate element. just do a simple assignment.
-// return values : NODE_OK , NODE_ILLEGAL_INDEX
-NodeResult nodeUpdateNodeElement(Node da, int id, NodeElement element);
+// return values : NODE_OK
+NodeResult nodeUpdateElement(Node node, NodeElement element);
 
 //------------------------------------------------------------------------------------------
 // display the element in the given index i.
 // this function uses displayNodeElement that is guaranteed by the user
 // no need to do here '\n'.
-// return values : NODE_OK, NODE_ILLEGAL_INDEX
-NodeResult nodeDisplayNodeElement(Node da, int i, displayNodeElement);
+// return values : NODE_OK
+NodeResult nodeDisplayElement(Node node, displayNodeElement);
 
 //------------------------------------------------------------------------------------------
-// display all the elements according to their position, from left to right.
-// each element is displayed in a single line and '\n at the end of the line.
-// the elements are displayed by using the nodeDisplayNodeElement function.
-void nodeDisplayAllNodeElements(Node);
-
-//------------------------------------------------------------------------------------------
-// returns the number of elements in the array.
-int size(Node);
-
-//------------------------------------------------------------------------------------------
-// deallocate all relevant memory and stop using the array.
+// deallocate all relevant memory and stop using the node
 void nodeDestroy(Node);
 
 //------------------------------------------------------------------------------------------
