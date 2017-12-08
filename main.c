f816ffb2faa01d2891011a8342414b9dcee7d128
Omer <omer.be@campus.technion.ac.il>
2017-12-08 14:09:21 +0200

    added user's functions for tests

--- main.c
+++ main.c
@@ -2,6 +2,35 @@
 // Created by Omer on 08/12/2017.
 //
 
+#include <stdlib.h>
+#include <stdio.h>
+#include "ListContainer/list_mtm.h"
+#include "ListContainer/node_mtm.h"
+
+NodeElement copyInt(NodeElement num) {
+    int *result = malloc(sizeof(*result));
+    if (result == NULL) return NULL;
+    *result = *(int *) num;
+    return (void *) result;
+}
+
+void freeInt(NodeElement num) {
+    free(num);
+}
+
+int compareInt(NodeElement num1, NodeElement num2, NodeSortKey key) {
+    if (*(int *) num1 == *(int *) num2) return 0;
+    if (*(int *) num1 > *(int *) num2) return 1;
+    else return -1;
+}
+
+bool filterInt(NodeElement num, NodeFilterKey key) {
+    if(*(int*)num >= *(int*)key) return true;
+    else return false;
+}
+
 int main() {
+
+
     return 0;
 }
