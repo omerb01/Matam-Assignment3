954afae6750af539683217741ee9aed7ca5e4146
Ilya <int3rsys@live.com>
2017-12-28 01:46:13 +0200

    New List adt added

---
+++ ListContainerV2/list_test.c
@@ -0,0 +1,321 @@
+//
+// Created by Ilya on 12/28/2017.
+//
+
+#include <stdio.h>
+#include "test_utilities.h"
+#include "list_mtm.h"
+#include <stdlib.h>
+
+
+int test_counter = 1;
+#define ASSERT(test_condition)  \
+   {if (!(test_condition)) {printf("TEST %d FAILED at line %d\n", \
+    test_counter, __LINE__); } \
+   else {printf("TEST %d OK at line %d\n", test_counter, __LINE__);} \
+    test_counter++;}
+
+ListElement copyInt(ListElement num) {
+    int *result = malloc(sizeof(int));
+    if (result == NULL) return NULL;
+    *result = *(int *) num;
+    return (void *) result;
+}
+
+void freeInt(ListElement num) {
+    free(num);
+}
+
+int compareInt(ListElement num1, ListElement num2, ListSortKey key) {
+    if (*(int *) num1 == *(int *) num2) return 0;
+    if (*(int *) num1 > *(int *) num2) return 1;
+    else return -1;
+}
+
+bool filterInt(ListElement num, ListFilterKey key) {
+    if (*(int *) num % *(int *) key == 0) return true;
+    else return false;
+}
+
+
+/*static bool testCreateList(){
+    List new_list = listCreate(copyInt, freeInt);
+    listDestroy(new_list);
+    return true;
+}
+
+static bool testCopyList(){
+    List new_list = listCreate(copyInt, freeInt);
+    List list = listCreate(copyInt, freeInt);
+
+    int *element = malloc(sizeof(*element));
+    *element = 1;
+    listInsertFirst(list, (void*)element);
+    *element = 2;
+    listInsertFirst(list, (void*)element);
+    *element = 3;
+    listInsertLast(list, (void*)element);
+    printList(list);
+    free(element);
+    listDestroy(list);
+    listDestroy(new_list);
+    return true;
+}*/
+
+int main() {
+    ListResult error;
+    int i;
+    int *element = malloc(sizeof(int));
+    printf("start testing list_mtm.c ... \n");
+
+    List list1;
+    list1 = listCreate(copyInt, NULL);
+    ASSERT(list1 == NULL);
+    list1 = listCreate(NULL, freeInt);
+    ASSERT(list1 == NULL);
+    list1 = listCreate(copyInt, freeInt);
+    ASSERT(listGetCurrent(list1) == NULL);
+    ASSERT(listGetFirst(list1) == NULL);
+    ASSERT(listGetNext(list1) == NULL);
+
+    List temp_list;
+    temp_list = listCopy(NULL);
+    ASSERT(temp_list == NULL);
+    temp_list = listCopy(list1);
+    ASSERT(listGetFirst(temp_list) == NULL);
+    ASSERT(listGetSize(temp_list) == 0);
+
+
+    *element = 1;
+    error = listInsertFirst(list1, NULL);
+    ASSERT(error == LIST_NULL_ARGUMENT);
+    error = listInsertFirst(NULL, element);
+    ASSERT(error == LIST_NULL_ARGUMENT);
+    error = listInsertFirst(list1, element);
+    ASSERT(error == LIST_SUCCESS);
+    ASSERT(*(int *) listGetFirst(list1) == 1);
+    ASSERT(*(int *) listGetCurrent(list1) == 1);
+    ASSERT(listGetNext(list1) == NULL);
+    ASSERT(listGetSize(list1) == 1);
+    *element = 999;
+    ASSERT(*(int *) listGetFirst(list1) == 1);
+
+    ASSERT(listGetFirst(temp_list) == NULL);
+    ASSERT(listGetSize(temp_list) == 0);
+    error = listRemoveCurrent(list1);
+    ASSERT(error == LIST_SUCCESS);
+    ASSERT(listGetCurrent(list1) == NULL);
+    ASSERT(listGetFirst(list1) == NULL);
+
+    *element = 2;
+    error = listInsertBeforeCurrent(list1, element);
+    ASSERT(error == LIST_INVALID_CURRENT);
+    error = listInsertAfterCurrent(list1, element);
+    ASSERT(error == LIST_INVALID_CURRENT);
+    error = listInsertLast(list1, NULL);
+    ASSERT(error == LIST_NULL_ARGUMENT);
+    error = listInsertLast(NULL, element);
+    ASSERT(error == LIST_NULL_ARGUMENT);
+    error = listInsertLast(list1, element);
+    ASSERT(error == LIST_SUCCESS);
+    ASSERT(*(int *) listGetFirst(list1) == 2);
+    ASSERT(*(int *) listGetCurrent(list1) == 2);
+    ASSERT(listGetNext(list1) == NULL);
+    ASSERT(listGetSize(list1) == 1);
+    *element = 999;
+    ASSERT(*(int *) listGetFirst(list1) == 2);
+
+    *element = 3;
+    error = listInsertLast(list1, element);
+    ASSERT(error == LIST_SUCCESS);
+    *element = 1;
+    error = listInsertFirst(list1, element);
+    ASSERT(error == LIST_SUCCESS);
+
+    listGetFirst(list1);
+    listGetNext(list1);
+
+    i = 1;
+    LIST_FOREACH(int*, current_num, list1) {
+        ASSERT(*current_num == i);
+        i++;
+    }
+    ASSERT(listGetCurrent(list1) == NULL);
+    ASSERT(listGetSize(list1) == 3);
+
+    listGetFirst(list1);
+    *element = 0;
+    error = listInsertBeforeCurrent(list1, NULL);
+    ASSERT(error == LIST_NULL_ARGUMENT);
+    error = listInsertBeforeCurrent(NULL, element);
+    ASSERT(error == LIST_NULL_ARGUMENT);
+    error = listInsertBeforeCurrent(list1, element);
+    ASSERT(error == LIST_SUCCESS);
+    ASSERT(*(int *) listGetCurrent(list1) == 1);
+    ASSERT(*(int *) listGetFirst(list1) == 0);
+    *element = 999;
+    ASSERT(*(int *) listGetFirst(list1) == 0);
+
+    i = listGetSize(list1);
+    LIST_FOREACH(int*, current_num, list1) {
+        if(i == 1) break;
+        i--;
+    }
+    ASSERT(*(int*)listGetCurrent(list1) == 3);
+
+    *element = 4;
+    error = listInsertAfterCurrent(list1, NULL);
+    ASSERT(error == LIST_NULL_ARGUMENT);
+    error = listInsertAfterCurrent(NULL, element);
+    ASSERT(error == LIST_NULL_ARGUMENT);
+    //printList(list1);
+    error = listInsertAfterCurrent(list1, element);
+    ASSERT(error == LIST_SUCCESS);
+    ASSERT(*(int *) listGetCurrent(list1) == 3);
+    ASSERT(*(int *) listGetNext(list1) == 4);
+    *element = 999;
+    ASSERT(*(int *) listGetCurrent(list1) == 4);
+
+    i = listGetSize(list1);
+    LIST_FOREACH(int*, current_num, list1) {
+        if(i == 3) break;
+        i--;
+    }
+    ASSERT(*(int*)listGetCurrent(list1) == 2);
+
+    *element = 999;
+    error = listInsertBeforeCurrent(list1, element);
+    ASSERT(error == LIST_SUCCESS);
+    error = listInsertAfterCurrent(list1, element);
+    ASSERT(error == LIST_SUCCESS);
+
+    i = listGetSize(list1);
+    LIST_FOREACH(int*, current_num, list1) {
+        if(i == 7) ASSERT(*current_num == 0);
+        if(i == 6) ASSERT(*current_num == 1);
+        if(i == 5) ASSERT(*current_num == 999);
+        if(i == 4) ASSERT(*current_num == 2);
+        if(i == 3) ASSERT(*current_num == 999);
+        if(i == 2) ASSERT(*current_num == 3);
+        if(i == 1) ASSERT(*current_num == 4);
+        i--;
+    }
+
+    List list2 = listCopy(list1);
+    ASSERT(list2 != NULL);
+
+    i = listGetSize(list2);
+    LIST_FOREACH(int*, current_num, list2) {
+        if(i == 7) ASSERT(*current_num == 0);
+        if(i == 6) ASSERT(*current_num == 1);
+        if(i == 5) ASSERT(*current_num == 999);
+        if(i == 4) ASSERT(*current_num == 2);
+        if(i == 3) ASSERT(*current_num == 999);
+        if(i == 2) ASSERT(*current_num == 3);
+        if(i == 1) ASSERT(*current_num == 4);
+        i--;
+    }
+    error = listSort(list1, NULL, NULL);
+    ASSERT(error == LIST_NULL_ARGUMENT);
+    error = listSort(NULL, compareInt, NULL);
+    ASSERT(error == LIST_NULL_ARGUMENT);
+    error = listSort(list1, compareInt, NULL);
+    ASSERT(error == LIST_SUCCESS);
+
+    i = listGetSize(list1);
+    LIST_FOREACH(int*, current_num, list1) {
+        if (i == 7) ASSERT(*current_num == 0);
+        if (i == 6) ASSERT(*current_num == 1);
+        if (i == 5) ASSERT(*current_num == 2);
+        if (i == 4) ASSERT(*current_num == 3);
+        if (i == 3) ASSERT(*current_num == 4);
+        if (i == 2) ASSERT(*current_num == 999);
+        if (i == 1) ASSERT(*current_num == 999);
+        i--;
+    }
+    i = listGetSize(list2);
+    LIST_FOREACH(int*, current_num, list2) {
+        if(i == 7) ASSERT(*current_num == 0);
+        if(i == 6) ASSERT(*current_num == 1);
+        if(i == 5) ASSERT(*current_num == 999);
+        if(i == 4) ASSERT(*current_num == 2);
+        if(i == 3) ASSERT(*current_num == 999);
+        if(i == 2) ASSERT(*current_num == 3);
+        if(i == 1) ASSERT(*current_num == 4);
+        i--;
+    }
+
+    LIST_FOREACH(int*, current_num, list1) {
+        if(*current_num == 999) {
+            error = listRemoveCurrent(list1); // removes one "999"
+            ASSERT(error == LIST_SUCCESS);
+        }
+    }
+    error = listRemoveCurrent(list1);
+    ASSERT(error == LIST_INVALID_CURRENT);
+
+    i = listGetSize(list1);
+    LIST_FOREACH(int*, current_num, list1) {
+        if(i == 6) ASSERT(*current_num == 0);
+        if(i == 5) ASSERT(*current_num == 1);
+        if(i == 4) ASSERT(*current_num == 2);
+        if(i == 3) ASSERT(*current_num == 3);
+        if(i == 2) ASSERT(*current_num == 4);
+        if(i == 1) ASSERT(*current_num == 999);
+        i--;
+    }
+
+    LIST_FOREACH(int*, current_num, list1) {
+        if(*current_num == 999) {
+            error = listRemoveCurrent(list1); // removes one "999"
+            ASSERT(error == LIST_SUCCESS);
+        }
+    }
+
+    i = listGetSize(list1);
+    LIST_FOREACH(int*, current_num, list1) {
+        if(i == 5) ASSERT(*current_num == 0);
+        if(i == 4) ASSERT(*current_num == 1);
+        if(i == 3) ASSERT(*current_num == 2);
+        if(i == 2) ASSERT(*current_num == 3);
+        if(i == 1) ASSERT(*current_num == 4);
+        i--;
+    }
+
+    int n = 2;
+    ListFilterKey key = &n;
+    List list3;
+    list3 = listFilter(list1, NULL, key);
+    ASSERT(list3 == NULL);
+    list3 = listFilter(NULL, filterInt, key);
+    ASSERT(list3 == NULL);
+    printList(list1);
+    list3 = listFilter(list1, filterInt, key);
+
+    i = listGetSize(list3);
+    LIST_FOREACH(int*, current_num, list3) {
+        if(i == 3) ASSERT(*current_num == 0);
+        if(i == 2) ASSERT(*current_num == 2);
+        if(i == 1) ASSERT(*current_num == 4);
+        i--;
+    }
+
+    i = listGetSize(list1);
+    LIST_FOREACH(int*, current_num, list1) {
+        if(i == 5) ASSERT(*current_num == 0);
+        if(i == 4) ASSERT(*current_num == 1);
+        if(i == 3) ASSERT(*current_num == 2);
+        if(i == 2) ASSERT(*current_num == 3);
+        if(i == 1) ASSERT(*current_num == 4);
+        i--;
+    }
+
+    listDestroy(temp_list);
+    listDestroy(list1);
+    listDestroy(list2);
+    listDestroy(list3);
+    free(element);
+
+
+    printf("FINISHED\n");
+}
---
+++ ListContainerV2/mtm_list.c
@@ -0,0 +1,305 @@
+//
+// Created by Ilya on 12/28/2017.
+//
+
+//
+// Created by Ilya on 12/24/2017.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "list_mtm.h"
+#include "assert.h"
+
+
+typedef struct node_t {
+    ListElement element;
+    struct node_t *next;
+} *Node;
+
+struct List_t {
+    Node head;
+    Node iterator;
+    CopyListElement copy;
+    FreeListElement free;
+};
+
+static ListResult
+allocateMemory(Node *new_node, ListElement element, List list) {
+    ListElement temp = list->copy(element);
+    if (temp == NULL) {
+        return LIST_OUT_OF_MEMORY;
+    }
+    *new_node = (Node) malloc(sizeof(**(new_node)));
+    if (*new_node == NULL) {
+        free(temp);
+        return LIST_OUT_OF_MEMORY;
+    }
+    (*new_node)->element = temp;
+    (*new_node)->next = NULL;
+}
+
+static void setOldIterator(Node old_iterator, List list) {
+    LIST_FOREACH(ListElement, iterator, list) {
+        if ((Node) iterator == old_iterator) break;
+    }
+}
+
+static void swapElements(Node *element1, Node *element2){
+    ListElement temp=(*element1)->element;
+    (*element1)->element=(*element2)->element;
+    (*element2)->element = temp;
+}
+
+static ListResult maxSort(List list, CompareListElements compareElement,
+                          ListSortKey key){
+    assert(list != NULL && compareElement != NULL);
+    Node temp_iterator=list->iterator, temp=list->iterator;
+    for(listGetFirst(list);list->iterator!=NULL;list->iterator = list->iterator->next){
+        for(temp=list->iterator->next; temp!=NULL; temp=temp->next){
+            if(compareElement(list->iterator->element,temp->element,key)>0){
+                swapElements(&(list->iterator),&(temp));
+            }
+            if(temp->next==NULL){
+                break;
+            }
+        }
+        if(list->iterator->next==NULL){
+            break;
+        }
+    }
+
+}
+
+List listCreate(CopyListElement copyElement, FreeListElement freeElement) {
+    if (copyElement == NULL || freeElement == NULL) return NULL;
+    List new_list = malloc(sizeof(*new_list));
+    if (new_list == NULL) return NULL;
+    Node head = malloc(sizeof(*head));
+    if (head == NULL) return NULL;
+    head->element = NULL;
+    head->next = NULL;
+    new_list->head = head;
+    new_list->iterator = new_list->head;
+    new_list->free = freeElement;
+    new_list->copy = copyElement;
+    return new_list;
+}
+
+List listCopy(List list) {
+    if (list == NULL) return NULL;
+    List copied_list = listCreate(list->copy, list->free);
+    Node temp_iterator = list->iterator;
+    ListResult error;
+    //TODO:clear
+    LIST_FOREACH(ListElement, iterator, list) {
+        error = listInsertLast(copied_list, iterator);
+        if (error == LIST_OUT_OF_MEMORY) {
+            listDestroy(copied_list);
+        } else if (list->iterator == temp_iterator) {
+            LIST_FOREACH(Node, copied_list_iterator, copied_list) {}
+        }
+    }
+    list->iterator = temp_iterator;
+    return copied_list;
+
+}
+
+int listGetSize(List list) {
+    if (list == NULL) return -1;
+    else if (list->head->element == NULL) return 0;
+    Node temp_iterator = list->iterator;
+    int counter = 0;
+    LIST_FOREACH(ListElement, current_element, list) {
+        counter++;
+    }
+
+    list->iterator = temp_iterator;
+    return counter;
+}
+
+ListElement listGetFirst(List list) {
+    assert(list != NULL);
+    if (list->head->element == NULL) {
+        list->iterator = NULL;
+        return NULL;
+    }
+    list->iterator = list->head;
+    return list->head->element;
+}
+
+ListElement listGetNext(List list) {
+    if (list == NULL) return NULL;
+    if (list->iterator == NULL) return NULL;
+
+    Node next_node = list->iterator->next;
+    if (next_node == NULL) {
+        list->iterator = NULL;
+        return NULL;
+    }
+    list->iterator = next_node;
+    return next_node->element;
+}
+
+ListElement listGetCurrent(List list) {
+    if (list == NULL || list->iterator == NULL) {
+        return NULL;
+    }
+    return list->iterator->element;
+}
+
+ListResult listInsertFirst(List list, ListElement element) {
+    if (list == NULL || element == NULL) return LIST_NULL_ARGUMENT;
+    Node new_node = NULL;
+    ListResult error;
+    error = allocateMemory(&new_node, element, list);
+    if (error == LIST_OUT_OF_MEMORY)
+        return LIST_OUT_OF_MEMORY;
+
+    if (list->head->element == NULL) {
+        list->head->element = new_node->element;
+        free(new_node);
+        return LIST_SUCCESS;
+    }
+    new_node->next = list->head;
+    list->head = new_node;
+    return LIST_SUCCESS;
+}
+
+ListResult listInsertLast(List list, ListElement element) {
+    ListResult error;
+    Node temp_iterator, new_node = NULL;
+    if (list == NULL || element == NULL) {
+        return LIST_NULL_ARGUMENT;
+    }
+    if (allocateMemory(&new_node, element, list) == LIST_OUT_OF_MEMORY)
+        return LIST_OUT_OF_MEMORY;
+    if (list->head->element == NULL) {
+        list->head->element = new_node->element;
+        free(new_node);
+        return LIST_SUCCESS;
+    }
+    temp_iterator = list->iterator;
+    for (list->iterator = list->head;
+         list->iterator->next != NULL; list->iterator = list->iterator->next) {}
+    list->iterator->next = new_node;
+    setOldIterator(temp_iterator, list);
+    return LIST_SUCCESS;
+}
+
+ListResult listInsertBeforeCurrent(List list, ListElement element) {
+    if (list == NULL || element == NULL) return LIST_NULL_ARGUMENT;
+    if (list->iterator == NULL) return LIST_INVALID_CURRENT;
+    if (list->iterator == list->head) {
+        listInsertFirst(list, element);
+        return LIST_SUCCESS;
+    }
+    Node new_node, temp_iterator;
+    if (allocateMemory(&new_node, element, list) == LIST_OUT_OF_MEMORY)
+        return LIST_OUT_OF_MEMORY;
+    temp_iterator = list->iterator;
+    for (listGetFirst(list); (list->iterator->next != NULL &&
+                              list->iterator->next !=
+                              temp_iterator); list->iterator = list->iterator->next)  {}
+    list->iterator->next = new_node;
+    new_node->next = temp_iterator;
+    list->iterator = temp_iterator;
+    return LIST_SUCCESS;
+}
+
+ListResult listInsertAfterCurrent(List list, ListElement element) {
+
+    if (list == NULL || element == NULL) {
+        return LIST_NULL_ARGUMENT;
+    } else if (list->iterator == NULL) {
+        return LIST_INVALID_CURRENT;
+    }
+    Node new_node, temp_iterator;
+    if (allocateMemory(&new_node, element, list) == LIST_OUT_OF_MEMORY)
+        return LIST_OUT_OF_MEMORY;
+    temp_iterator = list->iterator->next;
+    list->iterator->next = new_node;
+    new_node->next = temp_iterator;
+    return LIST_SUCCESS;
+}
+
+ListResult listRemoveCurrent(List list) {
+    if (list == NULL) return LIST_NULL_ARGUMENT;
+    if (list->iterator == NULL) return LIST_INVALID_CURRENT;
+    Node temp_node, temp_iterator;
+    if (list->iterator == list->head) {
+        if (list->head->next == NULL) {
+            list->free(list->head->element);
+            list->head->element = NULL;
+        } else {
+            temp_node = list->head->next;
+            list->free(list->head->element);
+            free(list->head);
+            list->head = temp_node;
+        }
+    } else {
+        temp_iterator = list->iterator;
+        for (listGetFirst(list); (list->iterator->next != NULL &&
+                                  list->iterator->next !=
+                                  temp_iterator); list->iterator = list->iterator->next) {}
+        temp_node = (list->iterator->next)->next;
+        list->free((list->iterator->next)->element);
+        free((list->iterator->next));
+        list->iterator->next = temp_node;
+    }
+    list->iterator = NULL;
+    return LIST_SUCCESS;
+}
+
+List listFilter(List list, FilterListElement filterElement, ListFilterKey key){
+    if (list == NULL || filterElement == NULL) return NULL;
+    ListResult list_error;
+    Node temp;
+    List list_copy = listCopy(list);
+    if (list_copy == NULL) return NULL;
+    listGetFirst(list_copy);
+    while(list_copy->iterator!=NULL){
+        if(filterElement(list_copy->iterator->element,key) == false){
+            list_error = listRemoveCurrent(list_copy);
+            list_copy->iterator = temp;
+            assert(list_error == LIST_SUCCESS);
+        }
+        temp=list_copy->iterator;
+        listGetNext(list_copy);
+    }
+    list_copy->iterator=NULL;
+    return list_copy;
+
+}
+
+ListResult listClear(List list) {
+    if (list == NULL) {
+        return LIST_NULL_ARGUMENT;
+    }
+    if (list->head->element == NULL) {
+        return LIST_SUCCESS;
+    } else {
+        listGetFirst(list);
+        do {
+            listRemoveCurrent(list);
+            listGetFirst(list);
+        } while (list->iterator != NULL);
+    }
+}
+
+ListResult
+listSort(List list, CompareListElements compareElement, ListSortKey key) {
+    if (list == NULL || compareElement == NULL) {
+        return LIST_NULL_ARGUMENT;
+    }
+    return maxSort(list, compareElement, key);
+}
+
+void listDestroy(List list) {
+    assert(list != NULL);
+    ListResult error;
+    error = listClear(list);
+    assert(error == LIST_SUCCESS);
+    list->free(list->head);
+    free(list);
+}
